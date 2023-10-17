27b4b8040b331c50f5c30e2637bc79f8d871b3a2
g0rb <vigilantesculpting@gmail.com>
2023-10-17 12:44:01 -0700

    improved update:

--- update.sh
+++ update.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -euvx
 
-git pull --rebase && ./main.py && git add -u :/ && git add docs && git commit -m "rebuild"
+# clean docs directory
+rm -rf docs && git checkout docs
+# rebase any upstream changes
+git pull --rebase
+# create the site
+./main.py
+# add all modified files
+git add -u :/ 
+# add the entire new docs directory
+git add docs
+# commit everything
+git commit -m "rebuild"
+
 # git push origin HEAD
 
 
